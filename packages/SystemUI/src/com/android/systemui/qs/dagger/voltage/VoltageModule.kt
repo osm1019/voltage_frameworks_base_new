7b7c82ab1a8f1dd77111c79f89abfbc0fb7ea8cd
stofstik <verstegen.daan@gmail.com>
2015-08-17 17:42:53 +0200

    SystemUI: Add tile to show volume panel
    Squashed:
    
        From: Joey Huab <joey@evolution-x.org>
        Date: Wed, 18 Mar 2020 16:23:51 +0000
        Subject: QS: Use Settings.Panel intent for Volume Tile
    
        Signed-off-by: Joey Huab <joey@evolution-x.org>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: Ic583c24e304d1edc903127237fc9eb5c7eeb7e4d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/voltage/VoltageModule.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/voltage/VoltageModule.kt
@@ -33,6 +33,7 @@ import com.android.systemui.qs.tiles.RefreshRateTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VPNTetheringTile
+import com.android.systemui.qs.tiles.VolumeTile
 import com.android.systemui.qs.tiles.VpnTile
 
 import dagger.Binds
@@ -138,6 +139,12 @@ interface VoltageModule {
     @IntoMap
     @StringKey(FPSInfoTile.TILE_SPEC)
     fun FPSInfoTile(fpsInfoTile: FPSInfoTile): QSTileImpl<*>
+
+    /** Inject VolumeTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(VolumeTile.TILE_SPEC)
+    fun bindVolumeTile(volumeTile: VolumeTile): QSTileImpl<*>
 
     /** Inject VpnTile into tileMap in QSModule */
     @Binds
