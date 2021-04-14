d8426415b7dd91ca2430024edd08b8fbc8b91d1e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-14 23:14:16 +0530

    display: Don't spam log when display state changes
     * dynamic refresh rate devices keep switching display configuration
       for changing refresh rate and ends up spamming log way too much
    
    [ghostrider-reborn]
    - DisplayManagerService -> DisplayDeviceRepository in android 12
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I57c19a0042083a86e86f2c210288904f6dc87e2c
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/display/DisplayDeviceRepository.java
+++ services/core/java/com/android/server/display/DisplayDeviceRepository.java
@@ -177,6 +177,7 @@ class DisplayDeviceRepository implements DisplayAdapter.Listener {
                         "handleDisplayDeviceChanged");
             }
             int diff = device.mDebugLastLoggedDeviceInfo.diff(info);
+            if (DEBUG) {
             if (diff == DisplayDeviceInfo.DIFF_STATE) {
                 Slog.i(TAG, "Display device changed state: \"" + info.name
                         + "\", " + Display.stateToString(info.state));
@@ -196,6 +197,7 @@ class DisplayDeviceRepository implements DisplayAdapter.Listener {
                 }
             } else if (diff != DisplayDeviceInfo.DIFF_HDR_SDR_RATIO) {
                 Slog.i(TAG, "Display device changed: " + info);
+                }
             }
 
             if ((diff & DisplayDeviceInfo.DIFF_COLOR_MODE) != 0) {
