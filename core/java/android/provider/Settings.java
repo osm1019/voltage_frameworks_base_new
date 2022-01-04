cfa13c8a8e9b219eee21df58560671525a2651d0
Kshitij Gupta <kshitijgm@gmail.com>
2022-01-04 16:10:15 +0530

    fwb: Screen off animations [1/2]
    Fade (default), CRT, and Scale
    
    Credit and respect to xplodwild for paving the way
    back in the KitKat days!
    
    History of the ElectronBeam class can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/kitkat/services/java/com/android/server/power/ElectronBeam.java
    
    *** Changes for Android 10 by bigrushdog ***
    Various updates needed for Surface related API in the ElectronBeam animation class
    
    [AgentFabulous | POSP]
    - Rebase and rewrite ElectronBeam class on top of ColorFade
    - Add usage of class-common Transaction instance
    - Add support for respecting wide-color and protected-content
    - Fixup for new API
    
    neobuddy89:
    Updated for Android 14.
    
    Change-Id: I58d269d44c901a8c0471807e3cf05c2054205d28
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6808,6 +6808,12 @@ public final class Settings {
          */
         public static final String FP_ERROR_VIBRATE = "fp_error_vibrate";
 
+	/**
+         * Defines the screen-off animation to display
+         * @hide
+         */
+        public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+
         /**
          * Whether to disable qs on secure lockscreen.
          * @hide
