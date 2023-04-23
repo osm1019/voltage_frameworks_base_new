7ad8944633830d1cef11d5b52236373878c092cd
nift4 <nift4@protonmail.com>
2023-04-23 19:14:43 +0200

    Allow disabling pointer acceleration
    Dhina17: Ported to 14
    
    Change-Id: I8e78ad6fa8b5361104af533e70a9c7e55a609611
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6980,6 +6980,11 @@ public final class Settings {
         public static final String LIVE_DISPLAY_HINTED = "live_display_hinted";
 
         /**
+         * @hide
+         */
+        public static final String PREVENT_POINTER_ACCELERATION = "prevent_pointer_acceleration";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
