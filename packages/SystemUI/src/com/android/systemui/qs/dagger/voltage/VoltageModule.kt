efa95b185438948d49899d3a3b7cfc4798c6e6ad
mydongistiny <jaysonedson@gmail.com>
2023-11-19 09:10:34 +0000

    QS: Add CPUInfo toggle tile @neobuddy89: Adapted tile for A13, A14.
    Change-Id: I84fcdff1c29c0bcd3f85d60b4ccba25288ebbddd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/voltage/VoltageModule.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/voltage/VoltageModule.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tiles.WeatherTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.AODTile
+import com.android.systemui.qs.tiles.CPUInfoTile
 import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.SoundTile
@@ -94,4 +95,11 @@ interface VoltageModule {
     @IntoMap
     @StringKey(AODTile.TILE_SPEC)
     fun bindAODTile(aodTile: AODTile): QSTileImpl<*>
+
+    /** Inject CPUInfoTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CPUInfoTile.TILE_SPEC)
+    fun CPUInfoTile(cpuInfoTile: CPUInfoTile): QSTileImpl<*>
+
 }
