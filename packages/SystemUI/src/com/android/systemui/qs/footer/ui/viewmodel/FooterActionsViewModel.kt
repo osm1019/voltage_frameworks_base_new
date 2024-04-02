7d2de8acf83ac366de411730ff826abf80c0ed48
Pranav Vashi <neobuddy89@gmail.com>
2024-04-02 21:19:26 +0530

    fixup! SystemUI: Launch power menu from QS only if enabled
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
@@ -25,7 +25,6 @@ import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import com.android.settingslib.Utils
-import com.android.systemui.Dependency
 import com.android.systemui.animation.Expandable
 import com.android.systemui.common.shared.model.ContentDescription
 import com.android.systemui.common.shared.model.Icon
@@ -116,6 +115,7 @@ class FooterActionsViewModel(
         private val globalActionsDialogLiteProvider: Provider<GlobalActionsDialogLite>,
         private val activityStarter: ActivityStarter,
         @Named(PM_LITE_ENABLED) private val showPowerButton: Boolean,
+        private val keyguardStateController: KeyguardStateController
     ) {
         /** Create a [FooterActionsViewModel] bound to the lifecycle of [lifecycleOwner]. */
         fun create(lifecycleOwner: LifecycleOwner): FooterActionsViewModel {
@@ -142,6 +142,7 @@ class FooterActionsViewModel(
                 globalActionsDialogLite,
                 activityStarter,
                 showPowerButton,
+                keyguardStateController,
             )
         }
     }
@@ -154,6 +155,7 @@ fun FooterActionsViewModel(
     globalActionsDialogLite: GlobalActionsDialogLite,
     activityStarter: ActivityStarter,
     showPowerButton: Boolean,
+    keyguardStateController: KeyguardStateController
 ): FooterActionsViewModel {
     suspend fun observeDeviceMonitoringDialogRequests(quickSettingsContext: Context) {
         footerActionsInteractor.deviceMonitoringDialogRequests.collect {
@@ -204,8 +206,7 @@ fun FooterActionsViewModel(
     }
 
     fun onPowerButtonClicked(expandable: Expandable) {
-        val mKeyguard = Dependency.get(KeyguardStateController::class.java)
-        if (mKeyguard.isShowing() && mKeyguard.isMethodSecure() 
+        if (keyguardStateController.isShowing() && keyguardStateController.isMethodSecure() 
                 && Settings.System.getIntForUser(appContext.getContentResolver(),
                 Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1, UserHandle.USER_CURRENT) == 0) {
             return
